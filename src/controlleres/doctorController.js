791a2e64d131d409915f23136b05dce2dab6174d
gabriela.ttavares <gabriela.ttavares@gmail.com>
2021-11-27 10:21:59 -0300

    :sparkles: Inserindo os endpoints Patch e Delete

--- src/controlleres/doctorController.js
+++ src/controlleres/doctorController.js
@@ -53,10 +53,39 @@ const updateDoctor = async (req, res) => {
         const rowsUpdated = await Doctor.update({ name, crm, specialty, clinic, phone, favorite }, {
             where: { id: doctorId }
         });
-        if (rowsUpdated && rowsUpdated > 0) {
-            res.status(200).send({ message: `${rowsUpdated[0]} medico(s) atualizado(s)` })
+        if (rowsUpdated && rowsUpdated[0] > 0) {
+            res.status(200).send({ message: `${rowsUpdated[0]} medico(s) atualizado(s) com a informacao de favorito` })
         } else {
             res.status(404).send({ message: `Medico com id ${doctorId} não encontrado para atualizar` })
+        }
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+}
+
+const updateFavorite = async (req, res) => {
+    const doctorId = req.params.id
+    const favorite = req.body.favorite
+    try {
+        const rowsUpdated = await Doctor.update({ favorite }, { where: { id: doctorId } });
+        if (rowsUpdated && rowsUpdated[0] > 0) {
+            res.status(200).send({ message: `${rowsUpdated[0]} medico(s) com informação de favorito atualizada com sucesso` })
+        } else {
+            res.status(404).send({ message: `Medico com id ${doctorId} não encontrado para atualizar informação de favorito` })
+        }
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+}
+
+const deleteDoctor = async (req, res) => {
+    const doctorId = req.params.id
+    try {
+        const rowsDeleted = await Doctor.destroy({ where: { id: doctorId } });
+        if (rowsDeleted) {
+            res.status(200).send({ message: `${rowsDeleted[0]} medico(s) deletado(s) com sucesso` })
+        } else {
+            res.status(404).send({ message: `Medico com id ${doctorId} não encontrado para deletar` })
         }
     } catch (error) {
         res.status(500).send({ message: error.message })
@@ -67,5 +96,7 @@ module.exports = {
     createDoctor,
     getAllDoctors,
     getDoctor,
-    updateDoctor
+    updateDoctor,
+    updateFavorite,
+    deleteDoctor
 }
