cc66ff02f7a8db28cc490f8dd802212f26bda1a7
gabriela.ttavares <gabriela.ttavares@gmail.com>
2021-11-27 13:13:29 -0300

    :green_heart: Inserindo o exemplo do Front-end

--- src/app.js
+++ src/app.js
@@ -1,26 +1,34 @@
-const express = require("express")
-const app = express()
-const doctors = require("./routes/doctors.js")
-const index = require("./routes/index.js")
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+const doctors = require("./routes/doctors")
+const index = require("./routes/index")
+
+app.use(express.json());
 
 app.use(function (req, res, next) {
-
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    )
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.use("/", index)
-app.use("/doctors", doctors)
+// para que o front consiga utilizar as rotas que vamos criar precisamos dar essa permissão de acesso
+app.options("/*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers")
+  res.header(
+    "Access-Control-Allow-Methods",
+    "PUT,POST,GET,DELETE,OPTIONS,PATCH"
+  );
+  res.send("send some thing whatever")
+})
 
+app.use("/", index)
+
+app.use("/doctors", doctors)
 
 module.exports = app;
 
-//module.exports = { 
-	//app
-//}
